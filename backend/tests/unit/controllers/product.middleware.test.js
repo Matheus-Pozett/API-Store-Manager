5c520e0a601303637a44d28d2f0f6df86b0362bd
Matheus Pozett <maa_souza11@hotmail.com>
2025-12-03 15:02:32 -0300

    test: testando middleware de produto

--- backend/tests/unit/controllers/product.middleware.test.js
+++ backend/tests/unit/controllers/product.middleware.test.js
@@ -31,4 +31,40 @@ describe('PRODUCT MIDDLEWARE', function () {
     expect(res.json).to.have.been.calledWith({ message: '"name" is required' });
     expect(next).to.not.have.been.called;
   });
+
+  it('Verifica se retorna erro em caso do body com name menor que 5 caracteres', function () {
+    const req = {
+      body: { name: 'test' },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    const next = sinon.stub().returns();
+
+    productMiddleware.validateNewProduct(req, res, next);
+
+    expect(res.status).to.have.been.calledWith(422);
+    expect(res.json).to.have.been.calledWith({ message: '"name" length must be at least 5 characters long' });
+    expect(next).to.not.have.been.called;
+  });
+
+  it('Em caso de campos válidos o next é chamado', function () {
+    const req = {
+      body: { name: 'teste' },
+    };
+
+    const res = {
+      status: sinon.stub().returnsThis(),
+      json: sinon.stub(),
+    };
+
+    const next = sinon.stub().returns();
+
+    productMiddleware.validateNewProduct(req, res, next);
+
+    expect(next).to.have.been.calledWith();
+  });
 });
